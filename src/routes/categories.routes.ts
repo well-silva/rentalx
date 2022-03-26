a0e248133adc270bee720e463744d41633d811f9
Wellington Silva <wellington.silva@captalys.com.br>
2022-03-26 15:48:56 -0300

    criando validacao de criacao de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already exists!" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
@@ -16,7 +22,7 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
   const allCategories = categoriesRepository.list();
 
-  return response.json({ allCategories });
+  return response.json(allCategories);
 });
 
 export { categoriesRoutes };
