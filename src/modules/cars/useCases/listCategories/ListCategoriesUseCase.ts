fc03822a793ebeaade608f65672ad9651949c078
Wellington Silva <well.silvaa97@gmail.com>
2023-02-21 05:54:08 -0300

    feat: testes de integracao

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,12 +1,12 @@
-import { inject, injectable } from "tsyringe";
+import { inject, injectable } from 'tsyringe';
 
-import { Category } from "@modules/cars/infra/typeorm/entities/Category";
-import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 
 @injectable()
 class ListCategoriesUseCase {
   constructor(
-    @inject("CategoriesRepository")
+    @inject('CategoriesRepository')
     private categoriesRepository: ICategoriesRepository
   ) {}
 
