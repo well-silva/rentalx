f5228e94df82e4df28459933fc7ed724664635da
Wellington Silva <well.silvaa97@gmail.com>
2023-03-12 15:34:17 -0300

    feat: test

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,6 +3,8 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
+  console.log('teste');
+
   return createConnection(
     Object.assign(defaultOptions, {
       database: process.env.NODE_ENV === 'test' ? 'rentalx_test' : defaultOptions.database,
