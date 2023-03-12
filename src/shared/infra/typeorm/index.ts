878a56d854ef4e39f915ab9dd141022cb82ef415
Wellington Silva <well.silvaa97@gmail.com>
2023-03-12 15:34:44 -0300

    refactor: remove console.log

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,8 +3,6 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
-  console.log('teste');
-
   return createConnection(
     Object.assign(defaultOptions, {
       database: process.env.NODE_ENV === 'test' ? 'rentalx_test' : defaultOptions.database,
